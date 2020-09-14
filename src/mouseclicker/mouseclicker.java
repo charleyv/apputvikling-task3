2185ac44c6f249d3553de50b75142511802b5a86
charleyv <charleyv@stud.ntnu.no>
2020-09-14 16:58:51 +0200

    Counter increments on click

--- src/mouseclicker/mouseclicker.java
+++ src/mouseclicker/mouseclicker.java
@@ -2,6 +2,8 @@ package mouseclicker;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -25,14 +27,14 @@ public class mouseclicker {
         JLabel infoTxt = new JLabel("See how many clicks you can do in a minute!");
         infoTxt.setBounds(SCREEN_WIDTH/2 - 150, SCREEN_HEIGHT/3 - 25, 300, 50);
 
-        JButton startTxt = new JButton("START");
-        startTxt.setBounds(SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/3 + 25, 300, 50);
-        startTxt.setFont(new Font("Verdana", Font.BOLD, 20));
+        JButton startBtn = new JButton("START");
+        startBtn.setBounds(SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/3 + 25, 300, 50);
+        startBtn.setFont(new Font("Verdana", Font.BOLD, 20));
 
-        JButton b = new JButton("CLICK");//creating instance of JButton
-        b.setFont(new Font("Verdana", Font.BOLD, 16));
-        b.setBounds(SCREEN_WIDTH/2 - 200,SCREEN_HEIGHT/2 - 20,100, 40);//x axis, y axis, width, height
-        b.setEnabled(false);
+        JButton clickBtn = new JButton("CLICK");//creating instance of JButton
+        clickBtn.setFont(new Font("Verdana", Font.BOLD, 16));
+        clickBtn.setBounds(SCREEN_WIDTH/2 - 200,SCREEN_HEIGHT/2 - 20,100, 40);//x axis, y axis, width, height
+        clickBtn.setEnabled(false);
 
         JLabel counter = new JLabel("COUNTER: 0");//creating instance of JButton
         counter.setFont(new Font("Verdana", Font.BOLD, 16));
@@ -42,16 +44,40 @@ public class mouseclicker {
         timeCounter.setBounds(SCREEN_WIDTH/2 - 200,SCREEN_HEIGHT - 300,200, 40);
         timeCounter.setFont(new Font("Verdana", Font.BOLD, 16));
 
+        startBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clickBtn.setEnabled(true);
+                startClick(timeCounter, counter, clickBtn);
+                startBtn.setEnabled(false);
+            }
+        });
+
+
         f.add(highScore);
         f.add(infoTxt); //adding text
-        f.add(startTxt);
-        f.add(b);   //adding button in JFrame
+        f.add(startBtn);
+        f.add(clickBtn);   //adding button in JFrame
         f.add(counter);
         f.add(timeCounter);
 
         f.setSize(SCREEN_WIDTH,SCREEN_HEIGHT);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
+    }
+
+    private static void startClick(JLabel timer, JLabel count, JButton clicker) {
+
+        clicker.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String[] counterText = count.getText().split("\\s");  // Get the current number of clicks
+                int amountOfClicks = Integer.parseInt(counterText[1]);  // Convert to int
+                ++amountOfClicks;  // Increment
+                count.setText("COUNTER: " + Integer.toString(amountOfClicks));  // Convert back to string
+            }
+        });
+
     }
 
     private static String checkHighScore() {
@@ -70,4 +96,6 @@ public class mouseclicker {
         }
         return line;
     }
+
+
 }
