08d5c2579370c64a581f5bf5e78fb796075e0e06
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2020-09-14 18:45:08 +0200

    refactored main function, removed redundant comments

--- src/GameCenter.java
+++ src/GameCenter.java
@@ -6,21 +6,24 @@ import javax.swing.*;
 public class GameCenter {
 
     public static void main(String[] args) {
-        JFrame f=new JFrame();//creating instance of JFrame
 
-        JButton b=new JButton("click");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
-        b.addActionListener(e -> {
-            RollADice.runGame();
-        });
-        f.add(b);//adding button in JFrame
-
-        f.setSize(400,500);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
+        // setting up the frame itself
+        JFrame frame = new JFrame();
+        frame.setSize(400,500);
+        frame.setLayout(null);  //using no layout managers
+        frame.setVisible(true);
 
         // This makes sure that game windows are closed if the main window is
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // buttons
+        JButton diceButton = new JButton("Roll a Dice");
+        diceButton.setBounds(130,100,100, 40);
+        diceButton.addActionListener(e -> {
+            RollADice.runGame();
+        });
+
+        frame.add(diceButton);
     }
 
 }
